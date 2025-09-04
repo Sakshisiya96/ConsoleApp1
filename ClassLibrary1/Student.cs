bb65fcbc1f6ca24c676e44de06e69abbc6eeaf2d
sakshi_jaiswal <sakshi1072000@gmail.com>
2025-09-03 21:25:05 -0700

    Add project files.

---
+++ ClassLibrary1/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace College
+{
+    public class Student
+    {
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Branch Branch { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using College;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.RollNumber = 101;   
+            student.Name = "John Doe";
+            student.Email = "john.doe@example.com"; 
+            student.Branch = new Branch();
+            student.Branch.BranchName = "Computer Science";
+            student.Branch.NoOfSemesters = 8;
+            Console.WriteLine($"Roll Number: {student.RollNumber}");
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Email: {student.Email}");
+            Console.WriteLine($"Branch Name: {student.Branch.BranchName}");
+            Console.WriteLine($"Number of Semesters: {student.Branch.NoOfSemesters}");
+        }
+    }
+}
