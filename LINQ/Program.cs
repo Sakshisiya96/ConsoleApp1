c8891c08a53dcd482efe97a3b751f870db418d77
sakshi_jaiswal <sakshi1072000@gmail.com>
2025-09-05 06:09:15 -0700

    LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -13,6 +13,10 @@ namespace LINQ
         public string Job { get; set; }
         public int Salary { get; set; }
     }
+    public class Person
+    {
+        public string PersonName { get; set; }
+    }
     public class Program
     {
         static void Main(string[] args)
@@ -26,17 +30,17 @@ namespace LINQ
                 new Employee(){EmpId=105,Name="Alexe",Job="Manager",Salary=8000},
             };//it will return IEnumerable<Employee>
             IEnumerable<Employee> managers = employees.Where(x => x.Job == "Manger").ToList();
-            var orderedEmployees = employees.OrderBy(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
-            var orderedEmployeesDesc = employees.OrderByDescending(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
-            var orderedSecondOrder = employees.OrderBy(emp=>emp.Job).ThenBy(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
+            var orderedEmployees = employees.OrderBy(emp => emp.Salary);//it will return IOrderedEnumerable<Employee>
+            var orderedEmployeesDesc = employees.OrderByDescending(emp => emp.Salary);//it will return IOrderedEnumerable<Employee>
+            var orderedSecondOrder = employees.OrderBy(emp => emp.Job).ThenBy(emp => emp.Salary);//it will return IOrderedEnumerable<Employee>
 
             //Where
             var ManagerFromEmployeeWithWhere = employees.Where(x => x.Job == "Manager").ToList();
-            Console.WriteLine("Where"+ManagerFromEmployeeWithWhere[0].EmpId);
+            Console.WriteLine("Where" + ManagerFromEmployeeWithWhere[0].EmpId);
 
             // First 
             Employee ManagerFirstEmployee = employees.First(x => x.Job == "Manager");
-            Console.WriteLine("First"+ ManagerFirstEmployee.EmpId);
+            Console.WriteLine("First" + ManagerFirstEmployee.EmpId);
 
             //FirstOrDefault
             Employee ManagerFirstOrDefaultEmployee = employees.FirstOrDefault(x => x.Job == "Clerk");
@@ -66,10 +70,64 @@ namespace LINQ
             {
                 Console.WriteLine("No clerk Found");
             }
-                foreach (var emp in managers)
+
+            //ElementAt
+            Employee empElementAt = employees.Where(x => x.Job == "Manager").ElementAt(1);
+            Console.WriteLine("ElementAt" + empElementAt.EmpId);
+
+            //ElementAtOrDefault
+            Employee empElementAtOrDefault = employees.Where(x => x.Job == "Manager").ElementAtOrDefault(5);
+            if (empElementAtOrDefault != null)
             {
-                Console.WriteLine($"Employee Name: {emp.Name}, Job: {emp.Job}, City: {emp.Salary}, ID: {emp.EmpId}"   );
+                Console.WriteLine("ElementAtOrDefault" + empElementAtOrDefault.EmpId);
+            }
+            else
+            {
+                Console.WriteLine("No ElementAtOrDefault Found");
+            }
+
+            //Single
+            Employee singleemployee = employees.Single(emp => emp.Job == "Hr");
+            Console.WriteLine("Single" + singleemployee.EmpId);
+
+            //SingleOrDefault(if no matching element then it will simply return null value)
+            //Single and SingleOrDefault will throw InvalidOperationException if more than one element is found
+
+            //Select
+            IEnumerable<double> result = employees.Select(emp => 10.8);//5 time 10.8
+
+           List<Person> re= employees.Select(emp => new Person() { PersonName = emp.Name }).ToList();
+            foreach (var it in re)
+            {
+                Console.WriteLine("Select" +" " + it.PersonName);
+            }
+
+            //foreach (var res in result)
+            //{
+            //    Console.WriteLine("Select" + res);
+            //}
+
+            //SingleOrDefault
+            Employee singleOrDefaultemployee = employees.SingleOrDefault(emp => emp.Job == "Clerk");
+            if (singleOrDefaultemployee != null)
+            {
+                Console.WriteLine("SingleOrDefault" + singleOrDefaultemployee.EmpId);
+            }
+            else
+            {
+                Console.WriteLine("No SingleOrDefault Found");
+            }
+                foreach (var emp in managers)
+                {
+                    Console.WriteLine($"Employee Name: {emp.Name}, Job: {emp.Job}, City: {emp.Salary}, ID: {emp.EmpId}");
+                }
+
+            //Min,Max,Sum,Average,Sum,Count
+            double emSum=employees.Sum(emp => emp.Salary);
+            double emAvg=employees.Average(emp => emp.Salary);
+            double emMax=employees.Max(emp => emp.Salary);
+            double emMin=employees.Min(emp => emp.Salary);
+            double emCount=employees.Count();
             }
         }
     }
-}
