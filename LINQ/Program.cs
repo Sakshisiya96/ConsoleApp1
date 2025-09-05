53aa837917c803ef187bd25f7e9a022a0086b8d2
sakshi_jaiswal <sakshi1072000@gmail.com>
2025-09-04 23:56:26 -0700

    LINQ Query(Where,OrderBy,First , FirstOrDefault ,Last,LastOrDefault)

---
+++ LINQ/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee(){EmpId=101,Name="Herry",Job="Designer",Salary=4838},
+                new Employee(){EmpId=102,Name="Jack",Job="Hr",Salary=5000},
+                new Employee(){EmpId=103,Name="Gabreile",Job="Software",Salary=6000},
+                new Employee(){EmpId=104,Name="Sak",Job="Manager",Salary=7000},
+                new Employee(){EmpId=105,Name="Alexe",Job="Manager",Salary=8000},
+            };//it will return IEnumerable<Employee>
+            IEnumerable<Employee> managers = employees.Where(x => x.Job == "Manger").ToList();
+            var orderedEmployees = employees.OrderBy(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
+            var orderedEmployeesDesc = employees.OrderByDescending(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
+            var orderedSecondOrder = employees.OrderBy(emp=>emp.Job).ThenBy(emp=>emp.Salary);//it will return IOrderedEnumerable<Employee>
+
+            //Where
+            var ManagerFromEmployeeWithWhere = employees.Where(x => x.Job == "Manager").ToList();
+            Console.WriteLine("Where"+ManagerFromEmployeeWithWhere[0].EmpId);
+
+            // First 
+            Employee ManagerFirstEmployee = employees.First(x => x.Job == "Manager");
+            Console.WriteLine("First"+ ManagerFirstEmployee.EmpId);
+
+            //FirstOrDefault
+            Employee ManagerFirstOrDefaultEmployee = employees.FirstOrDefault(x => x.Job == "Clerk");
+            if (ManagerFirstOrDefaultEmployee != null)
+            {
+                Console.WriteLine("Clerk " + ManagerFirstOrDefaultEmployee.EmpId);
+
+            }
+            Console.WriteLine("No clerk Found");
+
+            //USing where
+            var ManagerLastEmployeeWithWhere = employees.Where(x => x.Job == "Manager").ToList();
+            Console.WriteLine("Where" + ManagerLastEmployeeWithWhere[ManagerLastEmployeeWithWhere.Count - 1].EmpId);
+
+
+            //Last
+            Employee ManagerLastEmployee = employees.Last(x => x.Job == "Manager");
+            Console.WriteLine("Last" + ManagerLastEmployee.EmpId);
+
+            //LastOrDefault
+            Employee ManagerLastOrDefaultEmployee = employees.LastOrDefault(x => x.Job == "Clerk");
+            if (ManagerLastOrDefaultEmployee != null)
+            {
+                Console.WriteLine("LastOrDefault" + ManagerLastOrDefaultEmployee.EmpId);
+            }
+            else
+            {
+                Console.WriteLine("No clerk Found");
+            }
+                foreach (var emp in managers)
+            {
+                Console.WriteLine($"Employee Name: {emp.Name}, Job: {emp.Job}, City: {emp.Salary}, ID: {emp.EmpId}"   );
+            }
+        }
+    }
+}
---
+++ ManyToOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using Company;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee() {
+                Id = 1,
+               Name = "Jane Doe",
+               Email = "jane@gmail.com"
+               
+            };
+            Employee em2 = new Employee()
+            {
+                Id = 2,
+                Name = "John Smith",
+                Email = "john@gmail.com"
+
+            };
+            Department dept = new Department()
+            {
+                DeptId = 101,
+                DeptName = "Human Resources"
+            };
+            em1.Department = dept;
+            em2.Department = dept;
+            Console.WriteLine($"Employee 1: {em1.Name}, Email: {em1.Email}, Department: {em1.Department.DeptName}");
+            Console.WriteLine($"Employee 2: {em2.Name}, Email: {em2.Email}, Department: {em2.Department.DeptName}");
+
+        }
+    }
+}
